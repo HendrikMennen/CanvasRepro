f3a7d5c2483969da40ea0eb10ebec7131d3d0b91
Hendrik Mennen <hendrik.mennen@hs-osnabrueck.de>
2019-09-21 15:06:48 +0200

    CanvasRepro

---
+++ CanvasRepro/CanvasRepro/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Avalonia.Threading;
+
+namespace CanvasRepro
+{
+    public class MainWindow : Window
+    {
+        Canvas canvas;
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            canvas = this.Find<Canvas>("Canvas");
+            IBrush stroke = Brushes.Blue;
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new System.TimeSpan(0, 0, 0, 0, 1);
+            int xOff = 0;
+            timer.Tick += (sender, args) =>
+            {
+                canvas.Children.Clear();
+                xOff++;
+                if (xOff > canvas.Width) xOff = 0;
+                for(int i = 0; i < canvas.Height; i++)
+                {
+                    canvas.Children.Add(new Line { StartPoint = new Point(0, i), EndPoint = new Point(xOff, i), Stroke = stroke, StrokeThickness = 5});
+                }
+            };
+
+            timer.Start();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ CanvasRepro/CanvasRepro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+
+namespace CanvasRepro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Make this work with GTK/Skia/Cairo depending on command-line args
+            // again.
+            BuildAvaloniaApp().Start<MainWindow>();
+        }
+
+        /// <summary>
+        /// This method is needed for IDE previewer infrastructure
+        /// </summary>
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .LogToDebug()
+                .UsePlatformDetect();
+    }
+}
